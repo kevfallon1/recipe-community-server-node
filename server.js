e991586b24d4eab7e9827de3bafb2b5904e66877
Nikhil Bolar <bolar.nikhil@gmail.com>
2021-04-08 15:16:29 -0400

    Changed CORS Stuff

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require('express')
 const app = express()
 require('dotenv').config({ path: '.env' });
-const bodyparser=require('body-parser');
-app.use(bodyparser.json());
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded
+({ extended: false }))
+app.use(bodyParser.json())
 const MONGO_DB_URI = process.env.MONGO_DB_URI;
 
 // configure CORS
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "http://recipe-community.herokuapp.com/");
+    res.header('Access-Control-Allow-Origin', ["http://localhost:3000", "https://recipe-community.herokuapp.com/", "https://recipe-community-node-server.herokuapp.com/"]);
     res.header('Access-Control-Allow-Headers',
                'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
                'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    app.use(function (req,res, next) {
+        res.header('Access-Control-Allow-Origin',
+                   'http://localhost:4200');
+        res.header('Access-Control-Allow-Headers',
+                   'Content-Type, X-Requested-With, Origin');
+        res.header('Access-Control-Allow-Methods',
+                   'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Credentials', 'true');
+        next();
+    });
+
     next();
 });
 
